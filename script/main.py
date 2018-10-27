5d7c996db6bc6c30566902053e21a248f86d1af3
Shun Yamashita <shun40tut@gmail.com>
2018-10-27 23:38:50 +0900

    サンプルMIDIデータを生成するメソッドを追加

--- script/main.py
+++ script/main.py
@@ -1,5 +1,41 @@
 from Midi import Midi
 
+def example():
+    """
+    ♪ スピッツ / 空も飛べるはず
+    """
+    midi = Midi()
+
+    midi.add_note('G3', '1:3:240', '1/8', 'f')
+    midi.add_note('C4', '1:4:0', '1/8', 'f')
+    midi.add_note('D4', '1:4:240', '1/8', 'f')
+
+    midi.add_note('E4', '2:1:0', '1/4', 'f')
+    midi.add_note('E4', '2:2:0', '1/4', 'f')
+    midi.add_note('D4', '2:3:0', '1/8', 'f')
+    midi.add_note('E4', '2:3:240', '1/8', 'f')
+    midi.add_note('F4', '2:4:0', '1/4', 'f')
+
+    midi.add_note('E4', '3:1:0', '1/4', 'f')
+    midi.add_note('C4', '3:2:240', '1/8', 'f')
+    midi.add_note('C4', '3:3:0', '1/2', 'f')
+
+    midi.add_note('A4', '4:1:0', '1/4', 'f')
+    midi.add_note('A4', '4:2:0', '1/4', 'f')
+    midi.add_note('B3', '4:3:0', '1/8', 'f')
+    midi.add_note('C4', '4:3:240', '1/8', 'f')
+    midi.add_note('D4', '4:4:0', '1/8', 'f')
+    midi.add_note('A4', '4:4:240', '1/8', 'f')
+
+    midi.add_note('A4', '5:1:0', '1/8', 'f')
+    midi.add_note('G4', '5:1:240', '1/8', 'f')
+    midi.add_note('E4', '5:2:0', '1/8', 'f')
+    midi.add_note('G4', '5:2:240', '1/4', 'f')
+    midi.add_note('F4', '5:3:240', '1/4', 'f')
+    midi.add_note('G4', '5:4:240', '1/8', 'f')
+
+    midi.write_to_file('output/soramotoberuhazu.mid')
+
 if __name__ == '__main__':
     midi = Midi()
 
